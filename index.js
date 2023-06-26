3074b7ef72c2459ed7da2f0f42091d002d7743da
Raymond <oetaray@gmail.com>
2023-06-26 16:38:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // Write your solution here!
 let cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(_jina){
-    cats.push(_jina)
+function destructivelyAppendCat(name){
+    cats.push('Ralph')
     return cats
 }
-function destructivelyPrependCat(_jina){
-    cats.unshift(_jina = "Bob")
-    return cats;
+function destructivelyPrependCat(name){
+    cats.unshift('Bob')
+    return cats
 }
 function destructivelyRemoveLastCat(){
-    cats.pop()
-    return cats;
+    cats.pop('Garfield')
+    return cats
 }
 function destructivelyRemoveFirstCat(){
-    cats.shift()
+    cats.shift('Milo')
     return cats;
 }
-function appendCat(_jina){
-    let catsCopy = [...cats, _jina]
-    return catsCopy;
+function appendCat(name){
+    let copyOfCats = [...cats, name]
+    return copyOfCats;
 }
-function prependCat(_jina){
-    let catsCopy = [_jina, ...cats]
-    return catsCopy;
-}
-function removeLastCat(){
-    let catsCopy = cats.slice(0, -1);
-    return catsCopy;
+function prependCat(name){
+    let copyOfCats = [name, ...cats];
+    return copyOfCats;
 }
 function removeFirstCat(){
-    let catsCopy = cats.slice(1);
-    return catsCopy;
+    let copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
+function removeLastCat(){
+    let copyOfCats = cats.slice(0, -1)
+    return copyOfCats;
 }
